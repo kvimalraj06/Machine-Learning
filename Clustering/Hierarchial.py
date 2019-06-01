2c4f355aa066b2a90f1006bea6463485df247584
vimalpri1106 <45193116+vimalpri1106@users.noreply.github.com>
2019-06-01 20:12:55 +0530

    Hierarchial Clustering(Agglomerative)

---
+++ Clustering/Hierarchial.py
@@ -0,0 +1,41 @@
+"""Hierarchial clustering is similar to the k-means sometimes it gives the same result as well.
+But the approach or method to form the group or cluster is alone different.In this dendrogram 
+graph is used to identify the no. of clusters and also it contains the memory of the hierarchial 
+clustering algorithm.
+"""
+
+# Hierarchical Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch# used to find the optimum number of clusters
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+# Fitting Hierarchical Clustering to the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)#to find the given customer belongs to which cluster
+
+# Visualising the clusters(this is used only for clustering in two dimension)
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
